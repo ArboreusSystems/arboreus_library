7c425eba164c0f24b1266cf8b1e632eba3315b73
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-01-09 18:23:57 +0300

    ARBLIB-71 Intermediate commit

---
+++ lib/a_time/src/a_time_now.c
@@ -0,0 +1,163 @@
+// -------------------------------------------------------------------
+// @author Alexandr KIRILOV
+// @copyright (C) 2018, http://arboreus.system
+// @doc Arboreus UNIX current time handler
+//
+// @end
+// Created : 12/23/2018 at 20:47
+// -------------------------------------------------------------------
+
+// System includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+
+// Application includes
+#include "../../constants/constants_general.h"
+#include "headers/a_time_now.h"
+#include "headers/a_time_now_handler.h"
+
+
+
+// --------------------------------------
+// UNIX Module
+
+int main(int Number, char *Arguments[]) {
+	
+	if (Number>1) {
+		if (!strcmp("microseconds",Arguments[1])) {
+			a_time_now_microseconds();
+		} else if (!strcmp("milliseconds",Arguments[1])){
+			a_time_now_milliseconds();
+		} else if (!strcmp("seconds",Arguments[1])){
+			a_time_now_seconds();
+		} else if (!strcmp("int",Arguments[1])){
+			a_time_now_int();
+		} else if (!strcmp("int_date",Arguments[1])){
+			a_time_now_int_date();
+		} else if (!strcmp("int_full",Arguments[1])){
+			a_time_now_int_full();
+		} else if (!strcmp("int_extend",Arguments[1])){
+			a_time_now_int_extend();
+		} else if (!strcmp("rfc_822",Arguments[1])){
+			a_time_now_rfc_822();
+		} else if (!strcmp("rfc_850",Arguments[1])){
+			a_time_now_rfc_850();
+		} else if (!strcmp("ansi",Arguments[1])){
+			a_time_now_ansi();
+		} else {
+			FAILURE_ERROR(1,"Wrong parameter");
+		}
+	} else {
+		a_time_now_microseconds();
+	}
+}
+
+
+
+// --------------------------------------
+// Module API
+
+
+// Return time in microseconds
+int a_time_now_microseconds(){
+	
+	long long int Microseconds = 0;
+	if (atnh_microseconds(&Microseconds) == EXIT_SUCCESS) {
+		printf("%lld",Microseconds);
+		SUCCESS;
+	} else {
+		FAILURE_ERROR(2,"Wrong microseconds");
+	}
+}
+
+
+// Return time in milliseconds
+int a_time_now_milliseconds(){
+	
+	long long int Milliseconds = 0;
+	if (atnh_milliseconds(&Milliseconds) == EXIT_SUCCESS) {
+		printf("%lld",Milliseconds);
+		SUCCESS;
+	} else {
+		FAILURE_ERROR(2,"Wrong milliseconds");
+	}
+}
+
+
+// Return time in seconds
+int a_time_now_seconds(){
+	
+	long long int Seconds = 0;
+	atnh_seconds(&Seconds);
+	printf("%lld",Seconds);
+	SUCCESS;
+}
+
+
+// Return time like integer
+int a_time_now_int(){
+	
+	long long Int = 0;
+	atnh_int(&Int);
+	printf("%lld",Int);
+	SUCCESS;
+}
+
+
+// Return date like integer
+int a_time_now_int_date(){
+	
+	long long Int_date = 0;
+	atnh_int_date(&Int_date);
+	printf("%lld",Int_date);
+	SUCCESS;
+}
+
+
+// Return full time like integer
+int a_time_now_int_full(){
+	
+	long long Int_full = 0;
+	atnh_int_full(&Int_full);
+	printf("%lld",Int_full);
+	SUCCESS;
+}
+
+
+// Return full time like integer
+int a_time_now_int_extend(){
+	
+	long long Int_extend = 0;
+	if (atnh_int_extend(&Int_extend) == EXIT_SUCCESS) {
+		printf("%lld",Int_extend);
+		SUCCESS;
+	} else {
+		FAILURE_ERROR(2,"Wrong int_extend");
+	}
+}
+
+
+// Return time in RFC822 format
+int a_time_now_rfc_822(){
+	
+	char *RFC_822;
+	if (atnh_rfc_822(&RFC_822) != EXIT_SUCCESS){FAILURE;};
+	printf("%s",RFC_822);
+	SUCCESS;
+}
+
+
+// Return time in RFC850 format
+int a_time_now_rfc_850(){
+	atnh_rfc_850();
+	SUCCESS;
+}
+
+
+// Return time in ANSI format
+int a_time_now_ansi(){
+	atnh_ansi();
+	SUCCESS;
+}
---
+++ lib/a_time/src/a_time_now_handler.c
@@ -0,0 +1,163 @@
+// -------------------------------------------------------------------
+// @author Alexandr KIRILOV
+// @copyright (C) 2018, http://arboreus.system
+// @doc Arboreus current time handler
+//
+// @end
+// Created : 12/24/2018 at 13:54
+// -------------------------------------------------------------------
+
+// System includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/timeb.h>
+
+// Application includes
+#include "../../constants/constants_general.h"
+#include "headers/a_time_now_handler.h"
+#include "headers/a_time_weekday.h"
+#include "headers/a_time_month.h"
+#include "headers/a_time_rfc822.h"
+#include "../../universal_c/src/headers/a_convert.h"
+#include "../../universal_c/src/headers/a_string.h"
+
+
+// Return time in microseconds
+int atnh_microseconds(long long int *Pointer){
+	
+	struct timeval Time;
+	if (!gettimeofday(&Time, NULL)) {
+		*Pointer =
+			((long long int)Time.tv_sec)*1000000ll +
+			(long long int)Time.tv_usec;
+	} else {
+		*Pointer = -1;
+	}
+	
+	SUCCESS;
+}
+
+
+// Return time in milliseconds
+int atnh_milliseconds(long long int *Pointer){
+	
+	struct timeb Time;
+	if (!ftime(&Time)) {
+		*Pointer =
+			((long long int)Time.time)*1000ll +
+			(long long int)Time.millitm;
+	} else {
+		*Pointer = -1;
+	}
+	
+	SUCCESS;
+}
+
+
+// Return time in seconds
+int atnh_seconds(long long int *Pointer){
+	
+	*Pointer = time(NULL);
+	
+	SUCCESS;
+}
+
+
+// Return time like integer
+int atnh_int(long long int *Pointer){
+	
+	time_t Time = time(NULL);
+	struct tm *Today;
+	Today = localtime(&Time);
+	*Pointer =
+		(long long int)Today->tm_hour*10000 +
+		(long long int)Today->tm_min*100 +
+		(long long int)Today->tm_sec;
+	
+	SUCCESS;
+}
+
+
+// Return date like integer
+int atnh_int_date(long long int *Pointer){
+	
+	time_t Time = time(NULL);
+	struct tm *Today;
+	Today = localtime(&Time);
+	*Pointer =
+		((long long int)Today->tm_year+1900)*10000 +
+		((long long int)Today->tm_mon+1)*100 +
+		Today->tm_mday;
+	
+	SUCCESS;
+}
+
+
+// Return full time like integer
+int atnh_int_full(long long int *Pointer){
+	
+	time_t Time = time(NULL);
+	struct tm *Today;
+	Today = localtime(&Time);
+	*Pointer =
+		((long long int)Today->tm_year+1900)*10000000000 +
+		((long long int)Today->tm_mon+1)*100000000 +
+		(long long int)Today->tm_mday*1000000 +
+		(long long int)Today->tm_hour*10000 +
+		(long long int)Today->tm_min*100 +
+		(long long int)Today->tm_sec;
+	
+	SUCCESS;
+}
+
+
+// Return full time like integer within extended precession
+int atnh_int_extend(long long int *Pointer){
+	
+	struct timeval Time;
+	if (!gettimeofday(&Time, NULL)) {
+		div_t Extend = div(Time.tv_usec,10);
+		const long int Seconds = Time.tv_sec;
+		struct tm *Today;
+		Today = localtime(&Seconds);
+		*Pointer =
+			((long long int)Today->tm_year+1900)*1000000000000000 +
+			((long long int)Today->tm_mon+1)*10000000000000 +
+			(long long int)Today->tm_mday*100000000000 +
+			(long long int)Today->tm_hour*1000000000 +
+			(long long int)Today->tm_min*10000000 +
+			(long long int)Today->tm_sec*100000 +
+			(long long int)Extend.quot;
+	} else {
+		*Pointer = -1;
+	}
+	
+	SUCCESS;
+}
+
+
+// Return time in RFC822 format "Sun, 06 Nov 1994 08:49:37 GMT"
+int atnh_rfc_822(char **RFC_822){
+	
+	time_t Time = time(NULL);
+	struct tm *Today;
+	Today = localtime(&Time);
+	char ***Output = &RFC_822;
+	if (atrfc_822_from_struct(*Today,*Output) != EXIT_SUCCESS){FAILURE;}
+	SUCCESS;
+}
+
+
+// Return time in RFC850 format "Sunday, 06-Nov-94 08:49:37 GMT"
+int atnh_rfc_850(){
+	printf("rfc_850");
+	SUCCESS;
+}
+
+
+// Return time in ANSI format "Sun Nov  6 08:49:37 1994"
+int atnh_ansi(){
+	printf("ansi");
+	SUCCESS;
+}

d741c00f798e94558d81c31745b106a606430184
Alexandr Kirilov <alexandr.kirilov@post.com>
2019-01-10 18:36:18 +0300

    ARBLIB-78 Reorganise a_test sources

--- lib/a_test/src/a_time/headers/a_test_time.h
+++ lib/a_test/src/a_time/headers/a_test_time.h
@@ -1,7 +1,7 @@
 // -------------------------------------------------------------------
 // @author Alexandr KIRILOV
 // @copyright (C) 2019, http://arboreus.system
-// @doc
+// @doc Arboreus testing a_time functionality headers
 //
 // @end
 // Created : 01/06/2019 at 18:51

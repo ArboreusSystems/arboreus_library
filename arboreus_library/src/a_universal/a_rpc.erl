e4a4fdeff5115f6b7d151fa959ab4c4e7815865a
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-11-02 22:52:37 +0300

    ARBLIB-111 Add getting node list to a_rpc module

--- arboreus_library/src/a_universal/a_rpc.erl
+++ arboreus_library/src/a_universal/a_rpc.erl
@@ -13,7 +13,9 @@
 -export([
 	test/0,
 	async_call/4,
-	call/4
+	call/4,
+	nodes/0,
+	hostname/0
 ]).
 
 
@@ -48,3 +50,44 @@ call(Node_name,Module,Function,Arguments) ->
 
 async_call(Node_name,Module,Function,Arguments) ->
 	rpc:yield(rpc:async_call(Node_name,Module,Function,Arguments)).
+
+
+%% ----------------------------
+%% @doc Return list of started nodes on current server
+-spec nodes() -> proplists:proplist().
+
+nodes() ->
+
+	HOST_NAME = hostname(),
+	[_EPMD | NODE_DESCRIPTIONS] = string:tokens(os:cmd("epmd -names"),"\n"),
+	NODES = [parse_node_description(DESCRIPTION) || DESCRIPTION <- NODE_DESCRIPTIONS],
+	[{list_to_atom(string:concat(string:concat(NODE_NAME,"@"),HOST_NAME)),PORT} || {NODE_NAME,PORT} <- NODES].
+
+
+%% ----------------------------
+%% @doc Check the node description and return term within node data
+-spec parse_node_description(NODE_DESCRIPTION) -> {nomatch,REPLY} | {NODE_NAME_STRING,PORT_NUMBER}
+	when
+		NODE_DESCRIPTION :: string(),
+		REPLY :: any(),
+		NODE_NAME_STRING :: string(),
+		PORT_NUMBER :: port().
+
+parse_node_description(NODE_DESCRIPTION) ->
+
+	PATTERN = "^name\ ([a-zA-Z_0-9\-]*)\ at port ([0-9]{1,5})$",
+	case re:run(NODE_DESCRIPTION,PATTERN,[dotall, ungreedy, {capture, all_but_first, list}]) of
+		{match,[NODE_NAME,PORT]} -> {NODE_NAME,list_to_integer(PORT)};
+		REPLY -> {nomatch,REPLY}
+	end.
+
+
+%% ----------------------------
+%% @doc Return full hostname of current host
+-spec hostname() -> string().
+
+hostname() ->
+
+	CMD_OUTPUT = os:cmd("hostname -f"),
+	{HOSTNAME,_} = lists:split(length(CMD_OUTPUT) - 1, CMD_OUTPUT),
+	HOSTNAME.

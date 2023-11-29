66156889c47ea97342f0426b86f1909ea07b963c
Alexandr Kirilov <alexandr.kirilov@post.com>
2023-11-29 19:04:03 +0300

    ARBLIB-120 Add a_otp_supervisor module

--- arboreus_library/src/a_otp/a_otp_supervisor.erl
+++ arboreus_library/src/a_otp/a_otp_supervisor.erl
@@ -13,7 +13,9 @@
 -export([
 	test/0,
 	find_child/3,
-	first_child/1
+	first_child/1,
+	find_and_remove_child/3,
+	remove_child/2
 ]).
 
 
@@ -64,4 +66,62 @@ find_child(_,_,_) ->
 first_child(SUPERVISOR_PID) when is_pid(SUPERVISOR_PID) ->
 
 	[CHILD_PID | _] = supervisor:which_children(SUPERVISOR_PID),
-	CHILD_PID.
+	CHILD_PID;
+
+first_child(_) ->
+
+	{error,wrong_parameters}.
+
+
+%% ----------------------------
+%% @doc Find and remove child from defined supervisor
+-spec find_and_remove_child(TYPE,SUPERVISOR_PID,CHILD_ID) -> ok | {error,REASON}
+	when
+		TYPE :: by_id | by_pid,
+		SUPERVISOR_PID :: pid(),
+		CHILD_ID :: pid() | term(),
+		REASON :: term().
+
+find_and_remove_child(by_id,SUPERVISOR_PID,CHILD_ID) ->
+
+	case a_otp_supervisor:find_child(by_id,SUPERVISOR_PID,CHILD_ID) of
+		{CHILD_ID,_,_,_} ->remove_child(SUPERVISOR_PID,CHILD_ID);
+		_ -> {error,no_child}
+	end;
+
+find_and_remove_child(by_pid,SUPERVISOR_PID,CHILD_PID) ->
+
+	case a_otp_supervisor:find_child(by_pid,SUPERVISOR_PID,CHILD_PID) of
+		{CHILD_ID,CHILD_PID,_,_} -> remove_child(SUPERVISOR_PID,CHILD_ID);
+		_ -> {error,no_child}
+	end;
+
+find_and_remove_child(_,_,_) ->
+
+	{error,wrong_parameters}.
+
+
+%% ----------------------------
+%% @doc Remove child from defined supervisor
+-spec remove_child(SUPERVISOR_PID,CHILD_ID) -> ok | {error,REASON}
+	when
+		SUPERVISOR_PID :: pid(),
+		CHILD_ID :: term(),
+		REASON :: term().
+
+remove_child(SUPERVISOR_PID,CHILD_ID) ->
+
+	case supervisor:terminate_child(SUPERVISOR_PID,CHILD_ID) of
+		ok ->
+			case supervisor:delete_child(SUPERVISOR_PID,CHILD_ID) of
+				ok ->
+					case a_otp_supervisor:find_child(by_id,SUPERVISOR_PID,CHILD_ID) of
+						false -> ok;
+						REPLY -> {error,{child_still_exists,REPLY}}
+					end;
+				{error,REASON} ->
+					{error,{child_not_deleted,REASON}}
+			end;
+		{error,REASON} ->
+			{error,{child_not_terminated,REASON}}
+	end.

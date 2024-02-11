3eddedfad36671e903ebf3bd1f44f93ed720f5ac
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-02-11 09:58:16 +0300

    ARBLIB-128 Add a_types_db

---
+++ arboreus_library/include/types/types_a_db.hrl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Alexandr Kirilov, https://alexandr.kirilov.me
+%%% @copyright (C) 2024, Arboreus, https://arboreus.systems
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. Feb 2024 09:49
+%%%-------------------------------------------------------------------
+-author("Alexandr Kirilov, https://alexandr.kirilov.me").
+
+
+%% ------------------------------------------
+%% DB types
+
+-type a_db_table_name() :: atom().

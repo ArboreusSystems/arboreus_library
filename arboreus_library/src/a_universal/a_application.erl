cb91e66a7dd9fe25cdbccf32bf7f7ebf5c2ab576
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-10-07 11:31:18 +0300

    ARBLIB-149 Add stopping application to a_application module

--- arboreus_library/src/a_universal/a_application.erl
+++ arboreus_library/src/a_universal/a_application.erl
@@ -19,7 +19,10 @@
 
 	test/0,
 
-	is_started/1,is_started/2
+	is_started/1,is_started/2,
+	stop/1,stop/2,
+	stop_by/4,stop_by/5,
+	stop_by_function/2,stop_by_function/3
 
 ]).
 
@@ -65,3 +68,103 @@ is_started(NODE_NAME,APPLICATION_NAME) when is_list(APPLICATION_NAME) ->
 is_started(NODE_NAME,APPLICATION_NAME) when is_atom(APPLICATION_NAME) ->
 
 	a_rpc:async_call(NODE_NAME,a_application,is_started,[APPLICATION_NAME]).
+
+
+%% ----------------------------
+%% @doc Stop application if started
+-spec stop(APPLICATION_NAME) -> OUTPUT
+	when
+		APPLICATION_NAME :: list() | atom(),
+		OUTPUT :: ok | {error,REASON},
+		REASON :: term().
+
+stop(APPLICATION_NAME) ->
+
+	case is_started(APPLICATION_NAME) of
+		{true,APPLICATION_NAME} -> application:stop(APPLICATION_NAME);
+		{false,APPLICATION_NAME} -> ok
+	end.
+
+
+%% ----------------------------
+%% @doc Stop application if started on defined node
+-spec stop(NODE_NAME,APPLICATION_NAME) -> OUTPUT
+	when
+		NODE_NAME :: node(),
+		APPLICATION_NAME :: list() | atom(),
+		OUTPUT :: ok | {error,REASON} | {badrpc,nodedown},
+		REASON :: term().
+
+stop(NODE_NAME,APPLICATION_NAME) ->
+
+	a_rpc:async_call(NODE_NAME,a_application,stop,[APPLICATION_NAME]).
+
+
+%% ----------------------------
+%% @doc Stop application if started by defined module and function within parameters
+-spec stop_by(APPLICATION_NAME,MODULE,FUNCTION,PARAMETERS) -> OUTPUT
+	when
+		APPLICATION_NAME :: list() | atom(),
+		MODULE :: atom(),
+		FUNCTION :: atom(),
+		PARAMETERS :: [any()],
+		OUTPUT :: ok | any().
+
+stop_by(APPLICATION_NAME,MODULE,FUNCTION,PARAMETERS) ->
+
+	case is_started(APPLICATION_NAME) of
+		{true,APPLICATION_NAME} -> erlang:apply(MODULE,FUNCTION,PARAMETERS);
+		{false,APPLICATION_NAME} -> ok
+	end.
+
+
+%% ----------------------------
+%% @doc Stop application if started by defined module and function within parameters
+%% on defined node
+-spec stop_by(NODE_NAME,APPLICATION_NAME,MODULE,FUNCTION,PARAMETERS) -> OUTPUT
+	when
+		NODE_NAME :: node(),
+		APPLICATION_NAME :: list() | atom(),
+		MODULE :: atom(),
+		FUNCTION :: atom(),
+		PARAMETERS :: [any()],
+		OUTPUT :: ok | any().
+
+stop_by(NODE_NAME,APPLICATION_NAME,MODULE,FUNCTION,PARAMETERS) ->
+
+	a_rpc:async_call(
+		NODE_NAME,
+		a_application,stop_by,[APPLICATION_NAME,MODULE,FUNCTION,PARAMETERS]
+	).
+
+
+%% ----------------------------
+%% @doc Stop application if started by defined function
+-spec stop_by_function(APPLICATION_NAME,FUNCTION) -> OUTPUT
+	when
+		APPLICATION_NAME :: list() | atom(),
+		FUNCTION :: fun((IN_APPLICATION_NAME) -> any()),
+		IN_APPLICATION_NAME :: APPLICATION_NAME,
+		OUTPUT :: ok | any().
+
+stop_by_function(APPLICATION_NAME,FUNCTION) ->
+
+	case is_started(APPLICATION_NAME) of
+		{true,APPLICATION_NAME} -> FUNCTION(APPLICATION_NAME);
+		{false,APPLICATION_NAME} -> ok
+	end.
+
+
+%% ----------------------------
+%% @doc Stop application if started on defined node by defined function
+-spec stop_by_function(NODE_NAME,APPLICATION_NAME,FUNCTION) -> OUTPUT
+	when
+		NODE_NAME :: node(),
+		APPLICATION_NAME :: list() | atom(),
+		FUNCTION :: fun((IN_APPLICATION_NAME) -> any()),
+		IN_APPLICATION_NAME :: APPLICATION_NAME,
+		OUTPUT :: ok | any() | {badrpc,nodedown}.
+
+stop_by_function(NODE_NAME,APPLICATION_NAME,FUNCTION) ->
+
+	a_rpc:async_call(NODE_NAME,a_application,stop_by_function,[APPLICATION_NAME,FUNCTION]).

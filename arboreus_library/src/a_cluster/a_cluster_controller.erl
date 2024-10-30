69095c996828eb3ac0acf5f3d832ab6bbe07745c
Alexandr Kirilov <alexandr.kirilov@post.com>
2024-10-30 19:39:55 +0300

    ARBLIB-154 Add getting cluster controller PIDs

--- arboreus_library/src/a_cluster/a_cluster_controller.erl
+++ arboreus_library/src/a_cluster/a_cluster_controller.erl
@@ -16,7 +16,13 @@
 
 %% API
 -export([
-	test/0
+
+	test/0,
+
+	pid_db/1,
+	pid_handler/1,
+	pid_monitor/1
+
 ]).
 
 
@@ -27,3 +33,61 @@
 test() -> ok.
 
 
+%% ----------------------------
+%% @doc Return Cluster Controller db PID
+-spec pid_db(SUPERVISOR_PID) -> {ok,DB_PID} | {error,REASON}
+	when
+		SUPERVISOR_PID :: pid(),
+		DB_PID :: pid(),
+		REASON :: term().
+
+pid_db(SUPERVISOR_PID) ->
+
+	case a_otp_supervisor:find_child(
+		by_id,SUPERVISOR_PID,?A_ID_CLUSTER_CONTROLLER_DB
+	) of
+		{?A_ID_CLUSTER_CONTROLLER_DB,DB_PID,_TYPE,_MODULES} ->
+			{ok,DB_PID};
+		false ->
+			{error,no_cluster_controller_db}
+	end.
+
+
+%% ----------------------------
+%% @doc Return Cluster Controller handler PID
+-spec pid_handler(SUPERVISOR_PID) -> {ok,HANDLER_PID} | {error,REASON}
+	when
+		SUPERVISOR_PID :: pid(),
+		HANDLER_PID :: pid(),
+		REASON :: term().
+
+pid_handler(SUPERVISOR_PID) ->
+
+	case a_otp_supervisor:find_child(
+		by_id,SUPERVISOR_PID,?A_ID_CLUSTER_CONTROLLER_HANDLER
+	) of
+		{?A_ID_CLUSTER_CONTROLLER_HANDLER,HANDLER_PID,_TYPE,_MODULES} ->
+			{ok,HANDLER_PID};
+		false ->
+			{error,no_cluster_controller_handler}
+	end.
+
+
+%% ----------------------------
+%% @doc Return Cluster Controller monitor PID
+-spec pid_monitor(SUPERVISOR_PID) -> {ok,MONITOR_PID} | {error,REASON}
+	when
+		SUPERVISOR_PID :: pid(),
+		MONITOR_PID :: pid(),
+		REASON :: term().
+
+pid_monitor(SUPERVISOR_PID) ->
+
+	case a_otp_supervisor:find_child(
+		by_id,SUPERVISOR_PID,?A_ID_CLUSTER_CONTROLLER_MONITOR
+	) of
+		{?A_ID_CLUSTER_CONTROLLER_MONITOR,MONITOR_PID,_TYPE,_MODULES} ->
+			{ok,MONITOR_PID};
+		false ->
+			{error,no_cluster_controller_monitor}
+	end.
--- arboreus_library/src/a_cluster/a_cluster_controller_sup.erl
+++ arboreus_library/src/a_cluster/a_cluster_controller_sup.erl
@@ -121,7 +121,7 @@ start_db(SUPERVISOR_PID) ->
 start_handler(SUPERVISOR_PID) ->
 
 	HANDLER = #{
-		id => ?A_ID_CLUSTER_CONNECTOR_HANDLER,
+		id => ?A_ID_CLUSTER_CONTROLLER_HANDLER,
 		start => {'a_cluster_controller_handler_gs','start_link',[
 			#a_cluster_controller_handler_state{}
 		]},

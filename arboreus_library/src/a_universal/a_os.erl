7f0eaed9b0e115c296a5222bcab031ccf558500e
Alexandr Kirilov <alexandr.kirilov@post.com>
2026-01-12 12:32:12 +0300

    ARBLIB-180 Add 'ps' shell command handler to a_os module

--- arboreus_library/src/a_universal/a_os.erl
+++ arboreus_library/src/a_universal/a_os.erl
@@ -20,7 +20,11 @@
 	test/0,
 
 	ls/1,ls_no_check/1,
-	ls_filtered/2,ls_filtered_no_check/2
+	ls_filtered/2,ls_filtered_no_check/2,
+
+	ps/1,
+	ps_for_pid/2,
+	ps_get_pid_memory/1,ps_get_pid_memory_physical/1,ps_get_pid_memory_virtual/1
 
 ]).
 
@@ -106,3 +110,88 @@ ls_filtered(PATH,FILTER) ->
 ls_filtered_no_check(PATH,FILTER) ->
 
 	string:tokens(os:cmd("ls " ++ PATH ++ " | grep " ++ FILTER),"\n").
+
+
+%% ----------------------------
+%% @doc Return result of 'ps' command
+-spec ps(PARAMETERS) -> RESULT
+	when
+		PARAMETERS :: a_utf_text_string(),
+		RESULT :: list().
+
+ps(PARAMETERS) ->
+
+	string:tokens(os:cmd(string:concat("ps ",PARAMETERS)),"\n").
+
+
+%% ----------------------------
+%% @doc Return result of 'ps' shell command for defined OS PID
+-spec ps_for_pid(PARAMETERS,PID_OS) -> RESULT
+	when
+		PARAMETERS :: a_utf_text_string(),
+		PID_OS :: a_utf_text_string() | pos_integer(),
+		RESULT :: list().
+
+ps_for_pid(PARAMETERS,PID_OS) when is_integer(PID_OS)->
+
+	ps_for_pid(PARAMETERS,"" ++ integer_to_list(PID_OS));
+
+ps_for_pid(PARAMETERS,PID_OS) when is_list(PARAMETERS),is_list(PID_OS) ->
+
+	ps(string:concat(PARAMETERS,string:concat(" -p ",PID_OS))).
+
+
+%% ----------------------------
+%% @doc Return process memory usage by PID
+-spec ps_get_pid_memory(PID_OS) -> {ok,PHYSICAL_MEMORY_KB,VIRTUAL_MEMORY_KB} | {error,REASON}
+	when
+		PID_OS :: pos_integer() | a_utf_text_string(),
+		PHYSICAL_MEMORY_KB :: pos_integer(),
+		VIRTUAL_MEMORY_KB :: pos_integer(),
+		REASON :: term().
+
+ps_get_pid_memory(PID_OS) ->
+
+	case ps_for_pid("-x -o rss,vsz",PID_OS) of
+		[_,MEMORY_VALUE_STRING] ->
+			[RSS_STRING_KB,VSZ_STRING_KB] = string:tokens(MEMORY_VALUE_STRING," "),
+			{ok,list_to_integer(RSS_STRING_KB),list_to_integer(VSZ_STRING_KB)};
+		[ERROR_RESULT] ->
+			{error,{ERROR_RESULT}}
+	end.
+
+
+%% ----------------------------
+%% @doc Return physical memory measure used by process by PID
+-spec ps_get_pid_memory_physical(PID_OS) -> {ok,PHYSICAL_MEMORY_KB} | {error,REASON}
+	when
+		PID_OS :: pos_integer() | a_utf_text_string(),
+		PHYSICAL_MEMORY_KB :: pos_integer(),
+		REASON :: term().
+
+ps_get_pid_memory_physical(PID_OS) ->
+
+	case ps_for_pid("-x -o rss",PID_OS) of
+		[_,MEMORY_VALUE_STRING] ->
+			{ok,list_to_integer(string:trim(MEMORY_VALUE_STRING))};
+		[ERROR_RESULT] ->
+			{error,{ERROR_RESULT}}
+	end.
+
+
+%% ----------------------------
+%% @doc Return virtual memory measure used by process by PID
+-spec ps_get_pid_memory_virtual(PID_OS) -> {ok,VIRTUAL_MEMORY_KB} | {error,REASON}
+	when
+		PID_OS :: pos_integer() | a_utf_text_string(),
+		VIRTUAL_MEMORY_KB :: pos_integer(),
+		REASON :: term().
+
+ps_get_pid_memory_virtual(PID_OS) ->
+
+	case ps_for_pid("-x -o vsz",PID_OS) of
+		[_,MEMORY_VALUE_STRING] ->
+			{ok,list_to_integer(string:trim(MEMORY_VALUE_STRING))};
+		[ERROR_RESULT] ->
+			{error,{ERROR_RESULT}}
+	end.
